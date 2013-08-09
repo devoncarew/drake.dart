d35877140aec84354d717196043ce5db9affc4d4
Adam Singer <financeCoding@gmail.com>
2013-08-09 08:27:09 -0700

    clean up and updates to get running in dartium

--- app/lib/filesystem_sdk.dart
+++ app/lib/filesystem_sdk.dart
@@ -5,7 +5,7 @@ import 'dart:async';
 import 'dart:html' as html;
 import 'dart:json' as json;
 
-import '../packages/chrome/chrome.dart' as chrome;
+import '../packages/chrome/app.dart' as chrome;
 
 import 'filesystem.dart';
 
@@ -18,43 +18,43 @@ void registerSdkFileSystem() {
 
 class SdkFileSystem extends FileSystem {
   _SdkFolder _root;
-  
+
   SdkFileSystem() {
     _root = new _SdkFolder.createRoot(this, 'sdk');
   }
-  
+
   FileSystemFolder get root => _root;
-  
+
   String get id => "sdk";
-  
+
   String pickle(FileSystemEntity entity) {
     return entity.id;
   }
-  
+
   Future<FileSystemEntity> unpickle(String pickle) {
     List<String> paths = pickle.split('/');
-    
+
     if (paths.isEmpty) {
       return new Future.value(null);
     }
-    
+
     paths.removeAt(0);
-    
+
     if (paths.isEmpty) {
       return new Future.value(root);
     } else {
       return _unpickle(root, paths);
     }
   }
-  
+
   Future<FileSystemEntity> _unpickle(FileSystemFolder folder, List<String> paths) {
     String fileName = paths.removeAt(0);
-    
+
     return folder.getChild(fileName).then((FileSystemEntity entity) {
       if (paths.isEmpty) {
         return new Future.value(entity);
       }
-      
+
       if (entity is FileSystemFolder) {
         return _unpickle((entity as FileSystemFolder), paths);
       } else {
@@ -62,7 +62,7 @@ class SdkFileSystem extends FileSystem {
       }
     });
   }
-  
+
   Future<String> readContents(String path) {
     return html.HttpRequest.getString(chrome.Runtime.getURL(path));
   }
@@ -71,15 +71,15 @@ class SdkFileSystem extends FileSystem {
 class _SdkEntity extends FileSystemEntity {
   _SdkFolder _parent;
   String _name;
-  
+
   _SdkEntity(this._parent, this._name);
-  
+
   String get name => _name;
-  
+
   _SdkFolder get parent => _parent;
-  
+
   SdkFileSystem get fileSystem => _parent.fileSystem;
-  
+
   String get id => path;
 
   String get path {
@@ -94,23 +94,23 @@ class _SdkEntity extends FileSystemEntity {
 class _SdkFolder extends _SdkEntity implements FileSystemFolder {
   SdkFileSystem rootFs;
   List<FileSystemEntity> children;
-  
+
   _SdkFolder(_SdkFolder parent, String name) : super(parent, name);
-  
+
   _SdkFolder.createRoot(SdkFileSystem fs, String name) : super(null, name) {
     this.rootFs = fs;
   }
-  
+
   SdkFileSystem get fileSystem {
     return rootFs != null ? rootFs : super.fileSystem;
   }
-  
+
   Future<List<FileSystemEntity>> getChildren() {
     if (children != null) {
       return new Future.value(children);
     } else {
       Completer completer = new Completer();
-      
+
       fileSystem.readContents(path + "/files.json").then((String value) {
         children = parseJson(value);
         completer.complete(children);
@@ -119,11 +119,11 @@ class _SdkFolder extends _SdkEntity implements FileSystemFolder {
         children = new List<FileSystemEntity>();
         completer.complete(children);
       });
-      
+
       return completer.future;
     }
   }
-  
+
   Future<FileSystemEntity> getChild(String name) {
     return getChildren().then((List<FileSystemEntity> children) {
       for (FileSystemEntity entity in children) {
@@ -131,15 +131,15 @@ class _SdkFolder extends _SdkEntity implements FileSystemFolder {
           return new Future.value(entity);
         }
       }
-      
+
       return new Future.value(null);
     });
   }
-  
+
   List<FileSystemEntity> parseJson(String jsonText) {
     var list = json.parse(jsonText);
     List<FileSystemEntity> results = new List<FileSystemEntity>();
-    
+
     for (String value in list) {
       if (value.endsWith('/')) {
         results.add(new _SdkFolder(this, value.substring(0, value.length - 1)));
@@ -147,17 +147,17 @@ class _SdkFolder extends _SdkEntity implements FileSystemFolder {
         results.add(new _SdkFile(this, value));
       }
     }
-    
+
     return results;
   }
 }
 
 class _SdkFile extends _SdkEntity implements FileSystemFile {
-  
+
   _SdkFile(_SdkFolder parent, String name) : super(parent, name);
-  
+
   bool get modifyable => false;
-  
+
   Future<String> readContents() {
     return fileSystem.readContents(path);
   }
--- app/lib/preferences_chrome.dart
+++ app/lib/preferences_chrome.dart
@@ -3,7 +3,7 @@ library preferences_chrome;
 
 import 'dart:async';
 
-import '../packages/chrome/chrome.dart' as chrome;
+import '../packages/chrome/app.dart' as chrome;
 
 import 'preferences.dart';
 
@@ -18,32 +18,32 @@ ChromePreferenceStore chromePrefsSync = new ChromePreferenceStore(chrome.storage
 class ChromePreferenceStore extends PreferenceStore {
   StreamController<PreferenceEvent> streamController =
       new StreamController<PreferenceEvent>();
-  
+
   chrome.StorageArea _storageArea;
-  
+
   ChromePreferenceStore(chrome.StorageArea storageArea) {
     this._storageArea = storageArea;
   }
-  
+
   Future<String> getValue(String key) {
     return _storageArea.get([key]).then((Map<String, String> map) {
       return new Future.value(map[key]);
     });
   }
-  
+
   Future<String> setValue(String key, String value) {
     Map<String, String> map = {};
     map[key] = value;
-    
+
     return _storageArea.set(map).then((chrome.StorageArea _) {
       streamController.add(new PreferenceEvent(this, key, value));
       return new Future.value(value);
     });
   }
-  
+
   void flush() {
-    
+
   }
-  
-  Stream<PreferenceEvent> get onPreferenceChange => streamController.stream;  
+
+  Stream<PreferenceEvent> get onPreferenceChange => streamController.stream;
 }
