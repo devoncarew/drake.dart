eb6fcdb73120a7e9dd2e0aea17287394f18972b7
devoncarew@gmail.com <devoncarew@gmail.com>
2013-08-30 11:44:04 -0700

    update the latest sdk; reduce compiler warnings

--- app/lib/workspace.dart
+++ app/lib/workspace.dart
@@ -13,71 +13,76 @@ import 'preferences.dart';
  * it contains are all top-level folders.
  */
 class Workspace implements Container {
+  // not used for Workspace
+  Container _parent = null;
+  // not used for Workspace
+  FileSystemEntity _fileEntry = null;
+
   List<Resource> _children = [];
   PreferenceStore _store;
-  
+
   Workspace(PreferenceStore preferenceStore) {
     this._store = preferenceStore;
   }
-  
+
   String get name => null;
-  
+
   Container get parent => null;
-  
+
   Future<Workspace> initialize() {
     // TODO:
-    
+
 //    return _store.getValue('resources').then((String str) {
 //      if (str != null) {
 //        for (String path in str.split(';')) {
 //          fileSystemManager.unpickle(pickle)
 //        }
 //      }
-      
+
       return new Future.value(this);
 //    });
   }
-  
+
   // TODO: use a future
   Resource link(FileSystemEntity entity) {
     List<Resource> children = getChildren();
-      
+
     if (entity is FileSystemFile) {
       children.add(new File(this, entity));
     } else {
       children.add(new Folder(this, entity));
     }
-    
+
     _flush();
   }
-  
+
   // TODO: use a future
   void unlink(Resource resource) {
     List<Resource> children = getChildren();
 
     children.remove(resource);
-    
+
     _flush();
   }
-  
+
   List<Resource> getChildren() {
     if (_children != null) {
       return _children;
     } else {
 //      return _store.getValue("resources").then((String value) {
 //        _children = new List<Resource>();
-//        
+//
 //        if (value == null) {
 //          return new Future.value(_children);
 //        }
-//        
+//
 //        return Future.forEach(value.split(';'), (String str) {
 //          return fileSystemManager.unpickle(str).then((FileSystemEntity entity) {
 //            if (entity is FileSystemFile) {
 //              _children.add(new File(this, entity));
 //            } else {
 //              _children.add(new Folder(this, entity));
-//            }            
+//            }
 //          });
 //        }).then((_) {
 //          return new Future.value(_children);
@@ -88,30 +93,32 @@ class Workspace implements Container {
       return _children;
     }
   }
-  
+
+  Folder get topLevelFolder => null;
+
   void _flush() {
     String token = _children.map((Resource r) {
       return fileSystemManager.pickle(r._fileEntry);
     }).join(';');
-    
+
     _store.setValue("resources", token);
   }
 }
 
 abstract class Container extends Resource {
   List<Resource> _children;
-  
+
   Container(Container parent, FileSystemEntity entry) : super(parent, entry);
-  
+
   List<Resource> getChildren() {
     if (_children != null) {
       return _children;
     } else {
 //      FileSystemFolder folder = _fileEntry as FileSystemFolder;
-//      
+//
 //      return folder.getChildren().then((List<FileSystemEntity> files) {
 //        _children = new List<Resource>();
-//        
+//
 //        for (FileSystemEntity file in files) {
 //          if (file is FileSystemFile) {
 //            _children.add(new File(this, file));
@@ -119,7 +126,7 @@ abstract class Container extends Resource {
 //            _children.add(new Folder(this, file));
 //          }
 //        }
-//        
+//
 //        return new Future.value(_children);
 //      });
       _children = [];
@@ -131,24 +138,24 @@ abstract class Container extends Resource {
 abstract class Resource {
   Container _parent;
   FileSystemEntity _fileEntry;
-  
+
   Resource(this._parent, this._fileEntry);
-  
+
   String get name => _fileEntry.name;
-  
+
   Container get parent => _parent;
-  
+
   /**
    * Returns the top-level folder. This can return null for loose files.
    */
   Folder get topLevelFolder {
     Container p = parent;
-    
+
     while (p != null) {
       if (p is Folder && ((p as Folder).isTopLevel)) {
         return (p as Folder);
       }
-      
+
       p = p.parent;
     }
   }
@@ -156,32 +163,32 @@ abstract class Resource {
 
 // TODO:
 class Folder extends Container {
-  
+
   Folder(Container parent, FileSystemEntity entry) : super(parent, entry);
-  
+
   bool get isTopLevel => parent is Workspace;
-  
+
 }
 
 // TODO:
 class File extends Resource {
 
   File(Container parent, FileSystemEntity entry) : super(parent, entry);
-  
+
   List<Marker> getMarkers() {
     // TODO:
-    
+
   }
-  
+
 }
 
 class Marker {
   File _file;
   String _message;
-  
+
   Marker(this._file, this._message);
-  
+
   File get file => _file;
-  
-  String get message => _message;  
+
+  String get message => _message;
 }
--- app/test/vm_sample.dart
+++ app/test/vm_sample.dart
@@ -9,24 +9,26 @@ VmConnection connection;
 
 void main() {
   int port = 5858;
-  
+
   List<String> args = new Options().arguments;
-  
+
   if (args.length == 1) {
     port = int.parse(args[0]);
   }
-  
+
   print('connecting to port $port...');
-  
+
   Socket.connect('127.0.0.1', port).then((Socket socket) {
     print('connected to debug server');
-    
+
+    // TODO: fix:
+    // The argument type 'Socket' cannot be assigned to the parameter type 'EventSink<String>'
     connection = new VmConnection(
-        socket.transform(createVmStreamTransformer()), socket);
+        socket.transform(createVmStreamTransformer()), socket as dynamic);
     connection.logging = true;
     connection.onEvent.listen(
         handleDebuggerEvent,
-        onDone: () => print('connection closed'));    
+        onDone: () => print('connection closed'));
   }).catchError((e) {
     print("unable to connect: $e");
   });
@@ -34,10 +36,10 @@ void main() {
 
 void handleDebuggerEvent(VmEvent event) {
   //print(event);
-  
+
   if (event is VmPausedEvent) {
     VmPausedEvent e = event as VmPausedEvent;
-    
+
     if (e.isPaused) {
       connection.getStackTrace(e.isolate).then((VmResponse response) {
         List<VmCallFrame> frames = response.callFrames;
